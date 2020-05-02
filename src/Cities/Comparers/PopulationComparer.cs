48ee0f98d0e00e4cc7c0e1b895a61c801b3e3b37
Ava <prokop.ava@gmail.com>
2020-05-02 11:18:20 -0500

    added project

---
+++ src/Cities/Comparers/PopulationComparer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace Cities.Comparers
+{
+    public class PopulationComparer : IComparer<City>
+    {
+        public int Compare(City x, City y)
+        {
+            // Since we want to sort strings, we can use the
+            // comparer for the built-in string class
+
+            return (y.Population - x.Population);
+        }
+    }
+}
